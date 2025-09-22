95234cc751c15139e6739a17c82f047c46f96e4a
Anya306 <anya.sannikova.1997@mail.ru>
2025-09-22 20:58:01 +0300

    дз

--- 1A.py
+++ 1A.py
@@ -1,4 +1,4 @@
-'''
+
 def fib(n):
     if n == 1:
         return 1
@@ -24,6 +24,46 @@ s = []
 k = int(input())
 print(*mn(k, s))
 # упражнение 2
+
+def gcd(a, b):
+    if a == b:
+        return a
+    elif a > b:
+        return gcd(a - b, b)
+    else:
+        return gcd(a, b - a)
+def Euclid(a, b):
+    p = 1
+    q = 0
+    r = 0
+    s = 1
+    while a != 0 and b != 0:
+        if a >= b:
+            a = a - b
+            p = p - r
+            q = q - s
+        else:
+            b = b - a
+            r = r - p
+            s = s - q
+    if a != 0:
+        x = p
+        y = q
+    else:
+        x = r
+        y = s
+    return x, y
+q = int(input())
+ans = []
+for _ in range(q):
+    a, b = map(int, input().split())
+    d = gcd(a, b)
+    (x, y) = Euclid(a, b)
+    ans.append((x, y, d))
+for elem in ans:
+    print(*elem)
+# упражнение 3
+
 
 a, b = map(str, input().split())
 a = int(a)
@@ -94,7 +134,30 @@ while cnt < n*m:
 print(mas)
 # упражнение 5
 
-'''
+
+n, m = map(int, input().split())
+a = []
+b = []
+for _ in range(n):
+    t = [int(elem) for elem in input().split()]
+    a.append(t[:-1])
+    b.append(t[-1])
+def det(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+    else:
+        res = 0
+        for i in range(len(matrix)):
+            newm = [[elem for j, elem in enumerate(row) if j != i] for row in matrix[1:]]
+            res += ((-1)**(i))*matrix[0][i]*det(newm)
+        return res
+roots = []
+delta = det(a)
+for i in range(n):
+    mat = [[elem if j != i else b[irow] for j, elem in enumerate(row)] for irow, row in enumerate(a)]
+    deltai = det(mat)
+    print(deltai / delta)
+# упражнение 7
 
 
 
