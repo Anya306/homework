2c039c66e29c1a819285df63e8cacf37dcd7818e
Anya306 <anya.sannikova.1997@mail.ru>
2025-10-27 21:22:56 +0300

    калькулятор прямой и обратной нотации

--- 1F.py
+++ 1F.py
@@ -1,10 +1,14 @@
 from collections import deque
 
-deque0 = deque(input())
-print(deque0)
+s = input()
+deque0 = deque(s)
+deque02 = deque(s)
 deque1 = deque()
+deque12 = deque()
+res2 = deque()
 res = []
 zn = ['-', '+', '*', '/']
+zn2 = ['-', '+', '*', '/', '(', ')']
 
 while len(deque0) > 0:
     if deque0[0] == '(':
@@ -43,3 +47,36 @@ while len(deque1) > 0:
     res.append(deque1[0])
     deque1.popleft()
 print(*res)
+
+while len(deque02) > 0:
+    if deque02[-1] == ')':
+        deque12.appendleft(deque02[-1])
+        deque02.pop()
+    elif deque02[-1] == '(':
+        deque02.pop()
+        while deque12[0] != ')':
+            res2.appendleft(deque12[0])
+            deque12.popleft()
+        deque12.popleft()
+    elif deque02[-1] in zn:
+        if len(deque12) > 0:
+            if (deque12[0] == '*' or deque12[0] == '/') and (deque02[-1] == '+' or deque02[-1] == '-'):
+                while len(deque12) > 0 and (deque12[0] == '*' or deque12[0] == '/'):
+                    res2.appendleft(deque12[0])
+                    deque12.popleft()
+                deque12.appendleft(deque02[-1])
+                deque02.pop()
+            else:
+                deque12.appendleft(deque02[-1])
+                deque02.pop()
+        else:
+            deque12.appendleft(deque02[-1])
+            deque02.pop()
+    else:
+        res2.appendleft(deque02[-1])
+        deque02.pop()
+
+while len(deque12) > 0:
+    res2.appendleft(deque12[0])
+    deque12.popleft()
+print(*res2)
