e2e020033f0235d648d62f56933fb2d19dbb3863
Anya306 <anya.sannikova.1997@mail.ru>
2026-03-09 23:27:56 +0300

    дз

--- 2E.py
+++ 2E.py
@@ -1,4 +1,3 @@
-
 n = int(input())
 st = []
 for _ in range(n):
@@ -9,7 +8,7 @@ for i in range(n):
     for j in range(i + 1, n):
         x = st[i][0] - st[j][0]
         y = st[i][1] - st[j][1]
-        dist = (x**2 + y**2) ** 0.5
+        dist = (x ** 2 + y ** 2) ** 0.5
         mas.append((dist, i, j))
 mas.sort()
 p = [0] * n
@@ -29,3 +28,33 @@ for i in range(len(mas)):
     else:
         print((mas[i][0] * 1000) // 1 / 1000)
     break
+#задача 1
+
+
+c = int(input())
+for _ in range(c):
+    n, m = map(int, input().split())
+    mas = []
+    for i in range(m):
+        x, y, t = map(int, input().split())
+        mas.append((x, y, t))
+    d = [10**9] * n
+    d[0] = 0
+    for i in range(n - 1):
+        for j in range(m):
+            x, y, t = mas[j]
+            if d[x] + t < d[y]:
+                d[y] = d[x] + t
+    flag = False
+    for j in range(m):
+        x, y, t = mas[j]
+        if d[x] + t < d[y]:
+            flag = True
+            break
+
+    if flag:
+        print("возможно")
+    else:
+        print("не возможно")
+#задача 2
+
